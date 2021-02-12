2c8c5b270f3021b39baf1e291a3501a7c267f5d3
Donkevin <kevinohiro@gmail.com>
2021-02-12 14:02:50 +0000

    used the import statement for time

--- tutorial.go
+++ tutorial.go
@@ -1,7 +1,15 @@
 package main
 
 import	"fmt"
+import t "time"
+
+
 
 func main(){
+	//print my first statement
 	fmt.Println("Hy my first golang program!")
+	//using go's import statement
+	fmt.Println(t.Now())
+
 }
+
