99f83a33a66ae98ea126e2f85a424b8b93da8d97
Donkevin <kevinohiro@gmail.com>
2021-02-13 21:35:35 +0000

    complete scanner example with conversion

--- console input/console.go
+++ console input/console.go
@@ -4,14 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	
+	"strconv"
 )
 
 func main(){
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Enter your name: ")
+	fmt.Printf("Enter the year you were born: ")
 	scanner.Scan()
-	input := scanner.Text()
 
-	fmt.Printf("You typed: %q", input)
+	//the below line converts a string to int, 10 is the base of the number its converting to and its 64 bits
+	//the _ stores the error and since we are not interested we use _
+	input, _ := strconv.ParseInt(scanner.Text(), 10 , 64)
+
+	fmt.Printf("You will be %d years old at the end of 2021", 2021-input)
 }
